555f4ed05c8f7d4612596de307af77eab828dd5a
Thomas VanderBruggen <thomas@tvanderbruggen.com>
2026-05-26 14:16:18 -0500

    Finish the WeatherSErviceTests

--- WiperCheck.Tests/WeatherServiceTests.cs
+++ WiperCheck.Tests/WeatherServiceTests.cs
@@ -17,21 +17,19 @@ public class WeatherServiceTests
         _apiResponse = JsonSerializer.Deserialize<WeatherApiResponse>(File.ReadAllText("Fixtures/ChicagoWeatherReport.json")) ?? throw new Exception("Failed to deserialize fixture");
     }
     [Fact]
-    public async Task GetWeather_ReturnsCorrectWeather()
+    public void GetWeather_ReturnsCorrectWeather()
     {
        
         var mockDateProvider = new Mock<IDateTimeProvider>();
-        mockDateProvider.Setup(p => p.Today).Returns(new DateTime(2026, 5, 26, 15, 0, 0));
+        mockDateProvider.Setup(p => p.Today).Returns(new DateTime(2026, 5, 26));
 
         var service = new WeatherService(new HttpClient(), new ConfigurationBuilder().Build(), mockDateProvider.Object);
         
-        var wr = service.MapToWeatherResult(new GeocodeLocation("Chicago", 41.85, -87.65), _apiResponse, mockDateProvider.Object.Today);
+        var wr = service.MapToWeatherResult(new GeocodeLocation("Chicago", 41.85, -87.65), _apiResponse, new DateTime(2026, 5, 26, 15, 0, 0));
         
-        
-        
-        var testGeo = new GeocodeLocation(
-            "Test", 38.89, -77.03); 
-        var result = await service.GetWeather(testGeo, DateTime.Now.AddHours(15));
-        var abc = "idk"; 
+        Assert.Equal(15, wr.UtcForecastTime.Hour);
+        Assert.Equal(WeatherCondition.Overcast, wr.Condition);
+        Assert.Equal(73, wr.FeelsLikeF);
+        Assert.Equal("Chicago", wr.Coordinate.DisplayName);
     }
 }
